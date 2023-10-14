83e95c1dcd0cef89adbe4adc90f2971a3cfdbbe6
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-14 18:21:02 -0500

    Error handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -8,6 +8,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductService')
 
   constructor(
 
@@ -22,8 +24,7 @@ export class ProductsService {
       await this.productRepository.save(product);
       return product;
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException('Ayuda!')
+      this.handlerDBExceptions(error);
     }
   }
 
@@ -41,5 +42,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handlerDBExceptions( error: any ){
+    if( error.code == '23505' )
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 }
