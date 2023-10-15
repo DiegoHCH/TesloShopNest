c18d3db2fe87a95d3312bd7d732c5f105df4565f
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-15 16:48:17 -0500

    Query Runner

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -21,6 +21,8 @@ export class ProductsService {
 
     @InjectRepository(ProductImage)
     private readonly productImagesRepository: Repository<ProductImage>,
+
+    private readonly dataSource : DataSource,
 
   ){}
 
@@ -90,13 +92,14 @@ export class ProductsService {
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
-    });
+
+    const {images, ...toUpdate} = updateProductDto
+
+    const product = await this.productRepository.preload({ id, ...toUpdate });
 
     if(!product) throw new NotFoundException(`Product with id:  "${id}" not found`);
+
+    const queryRunner = this.dataSource.createQueryRunner();
     try {
       await this.productRepository.save(product);
       return product;
