f3fbce77ebc9ad57c3cd7e0477123fa1e9e8030e
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-17 14:20:41 -0500

    User login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
 import { User } from './entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -35,6 +35,24 @@ export class AuthService {
       this.handlerDBExceptions(error);
     }
 
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select: {email: true, password: true}
+    });
+
+    if(!user)
+      throw new UnauthorizedException('Credentials are not valid (email)');
+
+    if(!bcrypt.compareSync(password, user.password))
+     throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
   }
 
 
