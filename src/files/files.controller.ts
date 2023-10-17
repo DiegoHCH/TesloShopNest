881f23be8043390d023109a9b9f09e4c9b7442ab
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-10-17 09:49:34 -0500

    Serve files in a controlled manner

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,22 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res : Response,
+    @Param('imageName') imageName: string
+  ){
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
@@ -22,9 +32,12 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    return ({
-      filename : file.originalname
-    });
+    const secureUrl = `${file.filename}`;
+
+
+    return {
+      secureUrl
+    };
   }
 
 
